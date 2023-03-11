622696cec3c9365faabe77fd2b7ecfef41954ace
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-03-11 23:47:48 +0900

    メッセージブローカーに接続するスケルトン実装を追加した。

--- payment-gateway/src/main/java/com/programacho/paymentgateway/CreditController.java
+++ payment-gateway/src/main/java/com/programacho/paymentgateway/CreditController.java
@@ -2,6 +2,7 @@ package com.programacho.paymentgateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,17 @@ public class CreditController {
 
     private final Logger log = LoggerFactory.getLogger(CreditController.class);
 
+    private final StreamBridge streamBridge;
+
+    public CreditController(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
     @PostMapping("/authorize")
     public String authorize() {
+        // TODO: リクエストボディとレスポンスボディをメッセージブローカー経由で記録する機能を実装する。
+        streamBridge.send("log-in-0", "クレジット与信を取得します。");
+
         log.info("クレジット与信を取得します。");
 
         return "ok";
